ba68f0684170c23f283183ac9ec3d7b1513bfabd
Jan-Otto Kröpke <mail@jkroepke.de>
2023-08-20 08:45:57 +0200

    Add AWS

--- src/main/java/io/github/jkroepke/demo/controller/AWS.java
+++ src/main/java/io/github/jkroepke/demo/controller/AWS.java
@@ -1,16 +1,20 @@
 package io.github.jkroepke.demo.controller;
 
+import io.awspring.cloud.autoconfigure.core.AwsClientBuilderConfigurer;
 import io.awspring.cloud.s3.S3Resource;
 import io.awspring.cloud.s3.S3Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.regions.providers.AwsRegionProvider;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.StsClientBuilder;
+import software.amazon.awssdk.services.sts.model.GetCallerIdentityResponse;
 
 import java.nio.charset.Charset;
 import java.util.Collections;
@@ -21,24 +25,37 @@ import java.util.Map;
 @RestController
 public class AWS {;
 
-    private final AwsCredentialsProvider awsCredentialsProvider;
+    private final AwsClientBuilderConfigurer awsClientBuilderConfigurer;
+    private final AwsRegionProvider regionProvider;
     private final S3Client s3Client;
     private final S3Template s3Template;
 
     @Autowired
-    AWS(AwsCredentialsProvider awsCredentialsProvider, S3Client s3Client, S3Template s3Template) {
-        this.awsCredentialsProvider = awsCredentialsProvider;
+    AWS(AwsClientBuilderConfigurer awsClientBuilderConfigurer, AwsRegionProvider regionProvider, S3Client s3Client, S3Template s3Template) {
+        this.awsClientBuilderConfigurer = awsClientBuilderConfigurer;
+        this.regionProvider = regionProvider;
         this.s3Client = s3Client;
         this.s3Template = s3Template;
     }
 
     @RequestMapping("/aws")
     public Map<String, Object> ping() {
-        List<String> bucket = s3Client.listBuckets().buckets()
-            .stream().map(Bucket::name).toList();
 
         HashMap<String, Object> result = new HashMap<>();
+
+        List<String> bucket = s3Client.listBuckets().buckets()
+            .stream().map(Bucket::name).toList();
         result.put("buckets", bucket);
+        result.put("region", regionProvider.getRegion().toString());
+
+        StsClientBuilder client = awsClientBuilderConfigurer.configure(StsClient.builder());
+        try (StsClient stsClient = client.build()) {
+            GetCallerIdentityResponse callerIdentity = stsClient.getCallerIdentity();
+
+            result.put("arn", callerIdentity.arn());
+            result.put("account", callerIdentity.account());
+            result.put("userId", callerIdentity.userId());;
+        }
         return result;
     }
 
