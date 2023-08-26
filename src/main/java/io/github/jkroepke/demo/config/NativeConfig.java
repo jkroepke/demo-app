88230b446ea3d65deee54c7472efee61608ee4a5
Jan-Otto Kröpke <mail@jkroepke.de>
2023-08-26 19:26:13 +0200

    Fix AWS

---
+++ src/main/java/io/github/jkroepke/demo/config/NativeConfig.java
@@ -0,0 +1,8 @@
+package io.github.jkroepke.demo.config;
+
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RegisterReflectionForBinding({})
+public class NativeConfig { }
